daa8bfbd356acb43b977b3c9e20d5678e081c873
Steven NAVE <stevennave@gmail.com>
2022-11-01 13:31:44 -0400

    first commit

---
+++ controllers/image.js
@@ -0,0 +1,31 @@
+const Clarifai = require('clarifai');
+
+const app = new Clarifai.App({
+  apiKey: 'ba82628c701c4bea91de5a9a949cda2a'
+ });
+
+ const handleApiCall = (req, res) => {
+  app.models
+  .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+  .then(data => {
+    res.json(data);
+  })
+  .catch(err => res.status(400).json('unable to work with API'));
+ }
+
+
+const handleImage = (req, res, db) => {
+	const { id } = req.body;
+  db('users').where('id','=', id)
+  .increment('entries', 1)
+  .returning('entries')
+  .then(entries => {
+    res.json(entries[0].entries);
+  })
+  .catch(err => res.status(400).json('unable to get entries'));
+}
+
+module.exports = {
+    handleImage,
+    handleApiCall
+}
