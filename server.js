b8fed1ca91c7beea67d74543dacb1ed4c1b31444
Steven NAVE <stevennave@gmail.com>
2022-11-03 13:08:48 -0400

    updating database

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
+    host : 'postgresql-aerodynamic-47890',
     port : 5432,
     user : 'postgres',
     password : 'Ziggumz',
