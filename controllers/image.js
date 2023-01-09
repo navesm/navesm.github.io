81103303d8d422e66f7f63890a0b0fc325cdc446
Steven NAVE <stevennave@gmail.com>
2023-01-09 08:33:09 -0500

    clarifai API modification

--- controllers/image.js
+++ controllers/image.js
@@ -6,7 +6,7 @@ const app = new Clarifai.App({
 
  const handleApiCall = (req, res) => {
   app.models
-  .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+  .predict(Clarifai.'53e1df302c079b3db8a0a36033ed2d15', req.body.input)
   .then(data => {
     res.json(data);
   })
